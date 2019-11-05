1fd4c9032060cc345216162c451094789c36c93e
Bruno Jutkoski <bjutkoski@MacBook-Pro-de-Bruno.local>
2019-11-04 23:59:26 -0300

    Loading da autenticação

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -8,14 +8,23 @@ const INITIAL_STATE = {
 };
 
 export default function auth(state = INITIAL_STATE, action) {
-  switch (action.type) {
-    case Types.SIGN_IN_SUCCESS: {
-      return produce(state, draft => {
+  return produce(state, draft => {
+    switch (action.type) {
+      case Types.SIGN_IN_REQUEST: {
+        draft.loading = true;
+        break;
+      }
+      case Types.SIGN_IN_SUCCESS: {
         draft.token = action.payload.token;
         draft.signed = true;
-      });
+        draft.loading = false;
+        break;
+      }
+      case Types.SIGN_FAILURE: {
+        draft.loading = false;
+        break;
+      }
+      default:
     }
-    default:
-      return state;
-  }
+  });
 }
